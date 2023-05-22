24b0fd7b2b6c00523a0708cf14bc78913e30bc08
Daan Jumelet <123594600+daan-dj@users.noreply.github.com>
2023-05-22 15:10:00 +0200

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
         "./src/**/*.{ts,tsx}",
     ],
     theme: {
+        color: {
+            'background': '#121212',
+            'surface': '#292929',
+            'error': '#DF5E78',
+            'secondary': '#00DEC6',
+            'primary-variant': '#3D00BB',
+
+        },
         extend: {},
     },
     plugins: [],

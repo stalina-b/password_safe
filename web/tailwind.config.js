d0b0c6cc4eb5d3bab848c715fd618e9555379048
Daan Jumelet <123594600+daan-dj@users.noreply.github.com>
2023-05-22 15:25:41 +0200

    removed static types.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,20 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{ts,tsx}",
-    ],
-    theme: {
-        color: {
-            'background': '#121212',
-            'surface': '#292929',
-            'error': '#DF5E78',
-            'secondary': '#00DEC6',
-            'primary-variant': '#3D00BB',
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    color: {
+      'background': '#121212',
+      'surface': '#292929',
+      'error': '#DF5E78',
+      'secondary': '#00DEC6',
+      'primary-variant': '#3D00BB',
 
-        },
-        extend: {},
     },
-    plugins: [],
+    extend: {},
+  },
+  plugins: [],
 }
+
 

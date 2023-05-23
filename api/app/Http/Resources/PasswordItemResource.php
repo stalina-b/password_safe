fe2e605c82bbfd58cccdade024cfa2bf0499290a
bjvanbemmel <beauvanbemmel@live.nl>
2023-05-23 11:04:13 +0200

    Finished basic CRUD. Started work on the PasswordEncrypter.

--- api/app/Http/Resources/PasswordItemResource.php
+++ api/app/Http/Resources/PasswordItemResource.php
@@ -17,6 +17,7 @@ class PasswordItemResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'password' => $this->password,
             'created_at' => $this->created_at,

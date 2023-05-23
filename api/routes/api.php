06068c84885b9a42cc2b8cc15f5602d1b22a0a55
Daan Jumelet <123594600+daan-dj@users.noreply.github.com>
2023-05-23 09:34:27 +0200

    feature/(19-22) Login / Register backend

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/login', function () {
+    return json_encode(["message" => "Please login to continue"]);
+})->name('login');

4db43a5d832304998f042e89f60a3d5d938076f3
bjvanbemmel <beauvanbemmel@live.nl>
2023-05-25 10:26:58 +0200

    Refactored security checks into separate middlewares.

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Categories\categoryController;
 use App\Http\Controllers\PasswordItemController;
 use App\Http\Controllers\Securuity\SecurityCheckController;
+use App\Http\Middleware\EnsureMasterPasswordIsValid;
+use App\Http\Middleware\EnsurePasswordDoesNotExist;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -38,9 +40,9 @@ Route::middleware('auth:sanctum')->group(function() {
 
     // PasswordItems
     Route::get('/passwords', [PasswordItemController::class, 'index']);
-    Route::get('/passwords/{passwordItem}', [PasswordItemController::class, 'show']);
-    Route::post('/passwords', [PasswordItemController::class, 'store']);
-    Route::put('/passwords/{passwordItem}', [PasswordItemController::class, 'update']);
+    Route::get('/passwords/{passwordItem}', [PasswordItemController::class, 'show'])->middleware(EnsureMasterPasswordIsValid::class, EnsurePasswordDoesNotExist::class);
+    Route::post('/passwords', [PasswordItemController::class, 'store'])->middleware(EnsureMasterPasswordIsValid::class, EnsurePasswordDoesNotExist::class);
+    Route::put('/passwords/{passwordItem}', [PasswordItemController::class, 'update'])->middleware(EnsureMasterPasswordIsValid::class);
     Route::delete('/passwords/{passwordItem}', [PasswordItemController::class, 'destroy']);
 
     //Security check

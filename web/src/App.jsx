7d95c25680d3931d1b9e69eb54b653b4957352ba
Daan Jumelet <123594600+daan-dj@users.noreply.github.com>
2023-05-26 15:19:35 +0200

    root handling

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard/" element={<Dashboard />} />
+                <Route path="/" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
     );

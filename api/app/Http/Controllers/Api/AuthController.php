e9f722d5d12943839a104c8ae24ba057a21d768d
Daan Jumelet <123594600+daan-dj@users.noreply.github.com>
2023-05-26 14:46:24 +0200

    auto add a test and test tiem

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Enums\UserRoleEnum;
+use App\Models\category;
+use App\Models\PasswordItem;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -43,12 +45,29 @@ class AuthController extends Controller
                 'role' => UserRoleEnum::USER,
                 'password' => Hash::make($request->password)
             ]);
+            // create a new test category
+            $category = category::create([
+                'name' => 'test',
+                'user_id' => $user->id,
+            ]);
+            // create a new test password item
+            $passwordItem = PasswordItem::create([
+                'user_id' => $user->id,
+                'title' => 'test',
+                'master_password' => base64_encode($request->password),
+                'name' => 'test',
+                'username' => 'test@test.com',
+                'password' => '7jHUYe22313qdw}YeqN|%',
+                'note' => 'test note',
+                'category_id' => $category->id,
+            ]);
             // find the user
             return response()->json([
                 'status' => true,
                 'message' => 'User Created Successfully',
                 'token' => $user->createToken("API TOKEN")->plainTextToken,
                 'user' => User::find($user->id),
+                'passwordItem' => $passwordItem,
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
